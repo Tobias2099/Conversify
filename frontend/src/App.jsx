cc639e889f58b3109daca53d2226f7a4354bb7ef
Tobias2099 <tobiaspong@gmail.com>
2024-08-17 10:07:36 -0400

    revamp styling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -19,27 +19,23 @@ function App() {
   return ( 
     <>
       <Title />
-      <div className="customize">
-        <h2>Select Learning Language</h2>
-        <div className="button-container">
-          <Button type="language" name="English" handleClick={levelClick}/>
-          <Button type="language" name="French" handleClick={levelClick}/>
-          <Button type="language" name="Spanish" handleClick={levelClick}/>
-        </div>
+      <h2>Select Learning <span>Language</span></h2>
+      <div className="button-container">
+        <Button type="language" name="English" handleClick={levelClick}/>
+        <Button type="language" name="French" handleClick={levelClick}/>
+        <Button type="language" name="Spanish" handleClick={levelClick}/>
       </div>
-
-      <div className="customize">
-        <h2>Select Language Proficiency</h2>
-        <div className="button-container">
-          <Button type="profiency" name="Beginner" handleClick={levelClick}/>
-          <Button type="profiency" name="Intermediate" handleClick={levelClick}/>
-          <Button type="profiency" name="Advanced" handleClick={levelClick}/>
-        </div>
+      <h2>Select Language <span>Proficiency</span></h2>
+      <div className="button-container">
+        <Button type="profiency" name="Beginner" handleClick={levelClick}/>
+        <Button type="profiency" name="Intermediate" handleClick={levelClick}/>
+        <Button type="profiency" name="Advanced" handleClick={levelClick}/>
       </div>
       
 
       <div id="start" className="button-container">
-        <Button name="START" style={{fontSize: '250%', height: '80px', border: '3.75px solid', width: '35%'}}/>
+        <Button name="START" style={{fontSize: '250%', height: '90px', border: '5px solid #FFC000', width: '35%', borderRadius: '15% / 100%', display: 'block'}}/>
+        <div id ="start-decoration"></div>
       </div>
     </>
   )
--- frontend/src/Components/Title.jsx
+++ frontend/src/Components/Title.jsx
@@ -16,29 +16,34 @@ function Title() {
     height: 'auto',              // Adjust height if necessary
   }*/
 
-  const titleStyle = {
+  const bannerStyle = {
     //backgroundColor: '#0B1957', // Dark blue background
     backgroundImage: "linear-gradient(to right, #002D62, #0066b2, #172D9D, #2a52be, #787CFE)",
     color: '#F8F3EA',             // White text color
     textAlign: 'center',        // Center align text
     padding: '20px',            // Add some padding
-    fontSize: '5em',            // Large font size
+    fontSize: '6.5em',            // Large font size
     fontFamily: 'Arial, Calibri, sans-serif', // Font style
     fontWeight: 'bold',
     padding: '3%',
     margin: '-1%',
-    marginBottom: '4%'
+    borderBottom: '5px solid #FFC000',
   }
 
   const bioStyle = { 
     color: '#D1E8FF',             // Light blue text color
     fontSize: '20px',           // Smaller font size
-    marginTop: '10px'          // Space between title and bio
+    marginTop: '10px',          // Space between title and bio
+    fontStyle: 'italic'
+  }
+
+  const titleStyle = {
+    textShadow: '2px 2px 0 #FFC000, -1px -1px 0 #FFC000, 1px -1px 0 #FFC000, -1px 1px 0 #FFC000', /* Border color and size */
   }
 
   return (
-    <div style={titleStyle}>
-        <div>Conversify</div>
+    <div style={bannerStyle}>
+        <div style={titleStyle}>Conversify</div> 
         <div style={bioStyle}>
             Your personal language conversation partner! <br/> 
             Record your speech, and let our AI spark dynamic conversations that boost your fluency. <br/> 
