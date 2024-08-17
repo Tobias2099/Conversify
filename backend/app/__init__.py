ef5ddfe195b37dc914291ee9d6888f98cb30a0d0
anubhavsengupta <34662860+anubhavsengupta@users.noreply.github.com>
2024-08-17 10:16:39 -0400

    Add /process endpoint for text generation

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,7 +1,7 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import os
-from . import generator, recognizer
-
+from .recognizer import text_to_speech
+from .generator import generate
 def create_app():
     app = Flask(__name__)
     API_KEY = os.getenv('API_KEY')
@@ -10,4 +10,16 @@ def create_app():
     def home():
         return f"Hi {API_KEY}"
     
+
+    # This takes an object {text: "", conversation_history: []} and returns a json object of same type.
+    @app.route('/process', methods=['POST'])
+    def process():
+        data = request.get_json()
+        user_text = data['text']
+        conversation_history = data['conversation_history']
+        print(user_text, conversation_history)
+        AI_Response, new_history = generate(user_text, conversation_history, API_KEY)
+        return {'text': AI_Response, 'new_history': new_history}
+
+
     return app
