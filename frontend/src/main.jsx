18ca9e2081a4388480d923f0e1ab1ef33164d23e
Tobias2099 <tobiaspong@gmail.com>
2024-08-16 23:42:12 -0400

    add button component

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import Conversation from './Conversation.jsx'
-import './index.css'
+import './Style/index.css'
 
 
 import {
