f339955ef3548043bfbae83bc2dc7b1040295353
Nithika <nithika.karuna@gmail.com>
2024-08-17 12:17:54 -0400

    Second page buttons

--- frontend/src/Conversation.jsx
+++ frontend/src/Conversation.jsx
@@ -1,20 +1,27 @@
-import React from 'react'
-import "./Style/Conversation.css"
-import Title from "./Components/Title.jsx"
-import Button from "./Components/Button.jsx"
+import React from 'react'; 
+import { useNavigate } from 'react-router-dom'; 
+import "./Style/Conversation.css"; 
+import Title from "./Components/Title.jsx"; 
+import Button from "./Components/Button.jsx"; 
 
 function Conversation() {
+  const navigate = useNavigate(); 
+
+  function backClick() { 
+    navigate('/');
+  }
 
   return (
     <>
       <div id="banner">
         <Title hasBio={false}/>
-        <Button name="Change language" style={{backgroundColor: '#FFC000', border: 'none', width: '18%', marginTop: '-1%', fontSize: '150%', height: '50px'}}/>
+        <Button name="Change Settings" style={{backgroundColor: '#FFC000', border: 'none', width: '18%', marginTop: '-1%', fontSize: '150%', height: '50px'}}  handleClick={backClick}/>
       </div>
       
       <div id="convo-btns">
         <Button name="Start Recording"/>
-        <button name="End Conversation" />
+        <Button name="End Conversation" />
+        <Button name="Show Transcript" />
       </div>
     </>
   )
